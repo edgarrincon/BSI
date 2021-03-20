eff8336e912b359ee80d525aa7ca03a266735f60
edgarrincon <edgarrinconmoranml@gmail.com>
2021-03-20 10:25:48 -0400

    validation ready yijaaaa

--- js/EmailJS.js
+++ js/EmailJS.js
@@ -1,29 +1,30 @@
 function sendMail(params) {
-  const contact_name = document.getElementById("contact_name").value;
-  const contact_email = document.getElementById("contact_email").value;
-  const contact_subject = document.getElementById("contact_subject").value;
-  const contact_tel = document.getElementById("contact_tel").value;
-  const contact_message = document.getElementById("contact_message").value;
+  const contact_name = document.getElementById("contact_name");
+  const contact_email = document.getElementById("contact_email");
+  const contact_subject = document.getElementById("contact_subject");
+  const contact_tel = document.getElementById("contact_tel");
+  const contact_message = document.getElementById("contact_message");
 
   if (
-    (contact_name,
-    contact_email,
-    contact_tel,
-    contact_subject,
-    contact_message == "")
+    (!contact_name.checkValidity(),
+    !contact_email.checkValidity(),
+    !contact_tel.checkValidity(),
+    !contact_subject.checkValidity(),
+    !contact_message.checkValidity())
   ) {
-    alert("Please fill the required fields");
+    alert("Hi 👋 Please fill in all required fields 🙏");
   } else {
     let userData = {
-      contact_name: contact_name,
-      contact_email: contact_email,
-      contact_subject: contact_subject,
-      contact_tel: contact_tel,
-      contact_message: contact_message,
+      contact_name: contact_name.value,
+      contact_email: contact_email.value,
+      contact_subject: contact_subject.value,
+      contact_tel: contact_tel.value,
+      contact_message: contact_message.value,
     };
+
     emailjs.send("GMAIL", "TEMPLATE", userData).then(function (res) {
       console.log("success", res.status);
     });
-    alert("Thank you! we will keep in touch");
+    alert("Thank you!🎉 we will keep in touch 🤝");
   }
 }
